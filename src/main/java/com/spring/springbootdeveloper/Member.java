6ecbaaee2a317ad7c4b5375798063d4b769ad711
leeheojin <asd123pz@naver.com>
2024-06-01 21:56:10 +0900

    feat: test

---
+++ src/main/java/com/spring/springbootdeveloper/Member.java
@@ -0,0 +1,20 @@
+package com.spring.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id",updatable=false)
+    private Long id;
+
+    @Column(name="name",nullable=false)
+    private String name;
+}
